9e40aa9a51bcf755c4f74c69af5db32b8850a216
Marcos V Silva <mrcsvs@outlook.com>
2025-07-09 18:40:04 -0300

    equals + hashCode

--- src/main/java/com/srover/dscommerce/entities/Order.java
+++ src/main/java/com/srover/dscommerce/entities/Order.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -84,5 +85,18 @@ public class Order {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
--- src/main/java/com/srover/dscommerce/entities/Product.java
+++ src/main/java/com/srover/dscommerce/entities/Product.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -89,5 +90,18 @@ public class Product {
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
