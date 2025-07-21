8d84fb8848327d3d8371f026e148ec50bf20e47c
Marcos V Silva <mrcsvs@outlook.com>
2025-07-21 19:53:10 -0300

    GET Products with Categories

--- src/main/java/com/srover/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/srover/dscommerce/dto/ProductDTO.java
@@ -5,6 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductDTO {
     private Long id;
     @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
@@ -17,17 +20,19 @@ public class ProductDTO {
     private Double price;
     private String imgUrl;
 
+    private List<CategoryDTO> categories = new ArrayList<>();
+
     public ProductDTO() {
     }
 
-    public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
+    public ProductDTO(Long id, String name, String description, Double price, String imgUrl, List<CategoryDTO> categories) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
         this.imgUrl = imgUrl;
+        this.categories = categories;
     }
-
 
     public ProductDTO(Product entity) {
         this.id = entity.getId();
@@ -35,6 +40,11 @@ public class ProductDTO {
         this.description = entity.getDescription();
         this.price = entity.getPrice();
         this.imgUrl = entity.getImgUrl();
+        this.categories = entity.getCategories().stream().map(CategoryDTO::new).toList();
+    }
+
+    public List<CategoryDTO> getCategories() {
+        return categories;
     }
 
     public Long getId() {
--- src/main/java/com/srover/dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/srover/dscommerce/repositories/ProductRepository.java
@@ -8,8 +8,14 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    // Parece que JOIN FETCH funciona com Pageable agora
     @Query("SELECT obj " +
             "FROM Product obj " +
+            "JOIN FETCH obj.categories " +
             "WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%',:name,'%'))")
     Page<Product> searchByName(String name, Pageable pageable);
+
+    // Forma onde o JOIN FETCH não funcionava com Pageable
+//    @Query("SELECT obj FROM Product obj JOIN FETCH obj.categories WHERE obj IN :products ")
+//    List<Product> searchProductsCategories(List<Product> products);
 }
