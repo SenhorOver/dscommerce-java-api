27a21d51ec91bda6d1a1f696f011521bbb971197
Marcos V Silva <mrcsvs@outlook.com>
2025-07-25 00:24:37 -0300

    Spring OAuth2 Security - Authorization Server + Resource Server

---
+++ src/main/java/com/srover/dscommerce/services/UserService.java
@@ -0,0 +1,36 @@
+package com.srover.dscommerce.services;
+
+import com.srover.dscommerce.entities.Role;
+import com.srover.dscommerce.entities.User;
+import com.srover.dscommerce.projections.UserDetailsProjection;
+import com.srover.dscommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+        if(result.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(result.getFirst().getPassword());
+        for(UserDetailsProjection projection : result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+
+        return user;
+    }
+}
